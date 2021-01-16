0fbeb77aa259bfbb63f53b3b9cadc66ddebb4a0c
Cyril Dénos <cyril.denos@gmail.com>
2021-01-16 13:22:48 +0100

    changed gallery images + add google analytics tag

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -18,7 +18,70 @@ export class Gallery extends Component {
                   <div className="hover-bg">
                     {" "}
                     <a
-                      href="img/portfolio/01-large.jpg"
+                      href="img/portfolio/gal-01-large.jpg"
+                      title="Meuble television"
+                      data-lightbox-gallery="gallery1"
+                    >
+                      <div className="hover-text">
+                        <h4>Meuble television</h4>
+                      </div>
+                      <img
+                        src="img/portfolio/gal-01-small.jpg"
+                        className="img-responsive"
+                        alt="Meuble television"
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a
+                      href="img/portfolio/gal-02-large.jpg"
+                      title="Biblioteque"
+                      data-lightbox-gallery="gallery1"
+                    >
+                      <div className="hover-text">
+                        <h4>Biblioteque</h4>
+                      </div>
+                      <img
+                        src="img/portfolio/gal-02-small.jpg"
+                        className="img-responsive"
+                        alt="Biblioteque"
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a
+                      href="img/portfolio/gal-03-large.jpg"
+                      title="Bureau"
+                      data-lightbox-gallery="gallery1"
+                    >
+                      <div className="hover-text">
+                        <h4>Bureau</h4>
+                      </div>
+                      <img
+                        src="img/portfolio/gal-03-small.jpg"
+                        className="img-responsive"
+                        alt="Bureau"
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a
+                      href="img/portfolio/gal-04-large.jpg"
                       title="Project Title"
                       data-lightbox-gallery="gallery1"
                     >
@@ -26,7 +89,7 @@ export class Gallery extends Component {
                         <h4>Lorem Ipsum</h4>
                       </div>
                       <img
-                        src="img/portfolio/01-small.jpg"
+                        src="img/portfolio/gal-04-small.jpg"
                         className="img-responsive"
                         alt="Project Title"
                       />{" "}
@@ -39,7 +102,7 @@ export class Gallery extends Component {
                   <div className="hover-bg">
                     {" "}
                     <a
-                      href="img/portfolio/02-large.jpg"
+                      href="img/portfolio/gal-05-large.jpg"
                       title="Project Title"
                       data-lightbox-gallery="gallery1"
                     >
@@ -47,7 +110,7 @@ export class Gallery extends Component {
                         <h4>Adipiscing Elit</h4>
                       </div>
                       <img
-                        src="img/portfolio/02-small.jpg"
+                        src="img/portfolio/gal-05-small.jpg"
                         className="img-responsive"
                         alt="Project Title"
                       />{" "}
@@ -60,70 +123,7 @@ export class Gallery extends Component {
                   <div className="hover-bg">
                     {" "}
                     <a
-                      href="img/portfolio/03-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Lorem Ipsum</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/03-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/04-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Lorem Ipsum</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/04-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/05-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Adipiscing Elit</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/05-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/06-large.jpg"
+                      href="img/portfolio/gal-06-large.jpg"
                       title="Project Title"
                       data-lightbox-gallery="gallery1"
                     >
@@ -131,7 +131,7 @@ export class Gallery extends Component {
                         <h4>Dolor Sit</h4>
                       </div>
                       <img
-                        src="img/portfolio/06-small.jpg"
+                        src="img/portfolio/gal-06-small.jpg"
                         className="img-responsive"
                         alt="Project Title"
                       />{" "}
@@ -144,7 +144,7 @@ export class Gallery extends Component {
                   <div className="hover-bg">
                     {" "}
                     <a
-                      href="img/portfolio/07-large.jpg"
+                      href="img/portfolio/gal-07-large.jpg"
                       title="Project Title"
                       data-lightbox-gallery="gallery1"
                     >
@@ -152,49 +152,7 @@ export class Gallery extends Component {
                         <h4>Dolor Sit</h4>
                       </div>
                       <img
-                        src="img/portfolio/07-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/08-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Lorem Ipsum</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/08-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/09-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Adipiscing Elit</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/09-small.jpg"
+                        src="img/portfolio/gal-07-small.jpg"
                         className="img-responsive"
                         alt="Project Title"
                       />{" "}
